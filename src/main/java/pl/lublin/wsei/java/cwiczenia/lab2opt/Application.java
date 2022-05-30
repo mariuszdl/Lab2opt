825fffc3bcde3d5b3968076807ee1de8103d03e8
Mariusz Dłubak <maniek.14.12.2@gmail.com>
2022-05-30 18:41:11 +0200

    Utworzenie GUI aplikacji

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab2opt/Application.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab2opt/Application.java
@@ -10,7 +10,8 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("sample.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 470);
+        stage.setResizable(false);
         stage.setTitle("Rysowanie sinusoidy");
         stage.setScene(scene);
         stage.show();
--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab2opt/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab2opt/Controller.java
@@ -1,14 +1,5 @@
 package pl.lublin.wsei.java.cwiczenia.lab2opt;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
