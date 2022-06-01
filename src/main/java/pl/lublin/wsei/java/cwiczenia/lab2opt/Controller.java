cbe48c3d795a36316a695741ace9bf7dc71c58dd
Mariusz Dłubak <maniek.14.12.2@gmail.com>
2022-06-01 10:57:04 +0200

    Tworzenie metody inicjalizującej oraz dodanie danych do wykresu

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab2opt/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab2opt/Controller.java
@@ -1,5 +1,33 @@
 package pl.lublin.wsei.java.cwiczenia.lab2opt;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
 
+    @FXML
+    private LineChart lineChart;
+    @FXML
+    private Slider aplitudeSlider, frequencySlider, phaseSlider, offsetSlider;
+    private XYChart.Series series;
+
+    @FXML
+    public void initialize() {
+        series = new XYChart.Series<>();
+        series.setName("sin");
+        lineChart.getData().add(series);
+        regenSin();
+    }
+
+    @FXML
+    private void suwakChanged(MouseEvent event) {
+        regenSin();
+    }
+
+    private void regenSin() {
+
+    }
 }
